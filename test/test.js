89af99114ab0d0c88984d0357a2bcf52d372c936
Helio Frota <00hf11@gmail.com>
2018-12-13 10:51:21 -0300

    semistandard

--- test/test.js
+++ test/test.js
@@ -1,27 +1,27 @@
-'use strict'
+'use strict';
 
 /* eslint-env jest */
 
-const lib = require('../lib/index.js')
+const lib = require('../lib/index.js');
 
 test('Should parse the result correctly.', () => {
-  expect.assertions(1)
+  expect.assertions(1);
   return lib.metadata().then(result => {
-    expect(JSON.parse(result)['4'].lts).toBe('Argon')
+    expect(JSON.parse(result)['4'].lts).toBe('Argon');
   }).catch(e => {
-    console.error(e)
-  })
-})
+    console.error(e);
+  });
+});
 
 test('Should output latest metadata.', () => {
-  expect.assertions(4)
+  expect.assertions(4);
   return lib.metadata().then(result => {
-    const jsonObject = JSON.parse(result)
-    expect(jsonObject['4'].lts).toBe('Argon')
-    expect(jsonObject['5'].lts).toBe(false)
-    expect(jsonObject['6'].lts).toBe('Boron')
-    expect(jsonObject['7'].lts).toBe(false)
+    const jsonObject = JSON.parse(result);
+    expect(jsonObject['4'].lts).toBe('Argon');
+    expect(jsonObject['5'].lts).toBe(false);
+    expect(jsonObject['6'].lts).toBe('Boron');
+    expect(jsonObject['7'].lts).toBe(false);
   }).catch(e => {
-    console.error(e)
-  })
-})
+    console.error(e);
+  });
+});
