adc0f784ab799b2f248ba55803fa3bdb821dc8be
Helio Frota <00hf11@gmail.com>
2018-12-13 09:43:19 -0300

    src: using standard style (#9)

--- bin/node-metadata.js
+++ bin/node-metadata.js
@@ -1,24 +1,24 @@
 #! /usr/bin/env node
 
-const program = require('commander');
-const lib = require('../lib/index.js');
+const program = require('commander')
+const lib = require('../lib/index.js')
 
 program
   .version(require('../package.json').version)
   .option('-i --images', 'Indicates what images to show.')
-  .parse(process.argv);
+  .parse(process.argv)
 
 if (program.args.length === 0 || (!program.images && !program.i)) {
-  program.help();
+  program.help()
 }
 
 if (program.images || program.i) {
   lib.metadata().then(result => {
-    const jsonObject = JSON.parse(result);
-    const output = [];
+    const jsonObject = JSON.parse(result)
+    const output = []
     program.args.forEach(a => {
-      output.push(jsonObject[a]);
-    });
-    console.log(JSON.stringify(output));
-  }).catch(console.error);
+      output.push(jsonObject[a])
+    })
+    console.log(JSON.stringify(output))
+  }).catch(console.error)
 }
--- test/test.js
+++ test/test.js
@@ -1,29 +1,28 @@
-'use strict';
+'use strict'
 
-const test = require('tape');
-const lib = require('../lib/index.js');
+const test = require('tape')
+const lib = require('../lib/index.js')
 
 test('Should parse the result correctly.', (t) => {
-  t.plan(1);
+  t.plan(1)
   lib.metadata().then(result => {
-    t.equal(JSON.parse(result)['4'].lts, 'Argon', 'Successfully parsed, Argon found.');
+    t.equal(JSON.parse(result)['4'].lts, 'Argon', 'Successfully parsed, Argon found.')
   }).catch(e => {
-    console.error(e);
-    t.fail();
-  });
-});
+    console.error(e)
+    t.fail()
+  })
+})
 
 test('Should output latest metadata.', (t) => {
-  t.plan(4);
+  t.plan(4)
   lib.metadata().then(result => {
-    const jsonObject = JSON.parse(result);
-    t.equal(jsonObject['4'].lts, 'Argon', 'Argon found.');
-    t.equal(jsonObject['5'].lts, false, 'Node 5 is unnamed.');
-    t.equal(jsonObject['6'].lts, 'Boron', 'Boron found.');
-    t.equal(jsonObject['7'].lts, false, 'Node 7 is unnamed.');
+    const jsonObject = JSON.parse(result)
+    t.equal(jsonObject['4'].lts, 'Argon', 'Argon found.')
+    t.equal(jsonObject['5'].lts, false, 'Node 5 is unnamed.')
+    t.equal(jsonObject['6'].lts, 'Boron', 'Boron found.')
+    t.equal(jsonObject['7'].lts, false, 'Node 7 is unnamed.')
   }).catch(e => {
-    console.error(e);
-    t.fail();
-  });
-});
-
+    console.error(e)
+    t.fail()
+  })
+})
